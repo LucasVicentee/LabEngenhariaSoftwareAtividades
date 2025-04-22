afd14cf69f0cbf00ef47a44eec21f42859f82502
Lucas Henrique Vicente dos Santos <156228411+LucasVicentee@users.noreply.github.com>
2025-04-22 11:05:38 -0300

    Inicio das atividades de Laboratório de Java Script

---
+++ src/Atv01.js
@@ -0,0 +1,14 @@
+import Letreiro from "./Letreiro";
+import Relogio from "./Relogio";
+import { Link } from "react-router-dom";
+
+export default function Atv01() {
+    return (
+    <>
+      <h1>Atividade 1</h1>
+      <Relogio />
+      <Letreiro />
+      <Link to="/">Voltar</Link>
+    </>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Letreiro from './Letreiro';
+import Relogio from './Relogio';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Relogio />
+    <Letreiro />
+  </>
+);
