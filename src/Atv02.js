15b6bed1b33a1b60062ec9ca89908af652af3f4a
LucasVicentee <lh201612@gmail.com>
2025-05-20 11:02:06 -0300

    Atualização semanas 1 e 2

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,9 +1,27 @@
-import {Link} from "react-router-dom";
+import React from "react";
+import ContPessoas from "./ContPessoas";
+import "./ContPessoas.css";
+import { Link } from "react-router-dom";
 
-export default function Atv02() {
-    return (
-    <>
-      <Link to="/">Voltar</Link>
-    </>
+function Atv02() {
+  return (
+    <div className="container">
+      <h1 className="title">Atividade 02</h1>
+
+        <Link 
+          to="/" 
+          className="link"
+          onMouseEnter={(e) => e.target.classList.add('hover-glow')}
+          onMouseLeave={(e) => e.target.classList.remove('hover-glow')}
+        >
+          <span className="link-text">← Voltar</span>
+          <span className="link-cursor">_</span>
+        </Link>
+
+      <ContPessoas />
+    </div>
+    
   );
 }
+
+export default Atv02;
--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,10 +1,32 @@
-import './Letreiro.css'
+import { useState, useEffect } from 'react';
+import "./Letreiro.css";
 
 export default function Letreiro() {
-    return (
-      <div>
-        <h1>Letreiro</h1>
-        <p>Meu letreiro</p>
-      </div>
-    );
-  }
+  const mensagemCompleta = 'Me chamo Lucas Henrique Vicente dos Santos, tenho 21 anos e estudo na Fatec de Presidente Prudente';
+  const [mensagemAtual, setMensagemAtual] = useState('');
+  const [letraAtual_index, setLetraAtual] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      if (letraAtual_index < mensagemCompleta.length) {
+        setMensagemAtual((textoAnterior) => textoAnterior + mensagemCompleta[letraAtual_index]);
+        setLetraAtual((indiceAnterior) => indiceAnterior + 1);
+      } else {
+        clearInterval(intervalo);
+        setTimeout(() => {
+          setMensagemAtual('');
+          setLetraAtual(0);
+        }, 1000);
+      }
+    }, 65);
+
+    return () => clearInterval(intervalo);
+  }, [letraAtual_index]);
+
+  return (
+    <section>
+      <h1>Letreiro</h1>
+      <h2>{mensagemAtual}</h2>
+    </section>
+  );
+}
