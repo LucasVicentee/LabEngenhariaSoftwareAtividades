7dac290e3cb5fce57140974943f8ad6d691e9a9a
LucasVicentee <lh201612@gmail.com>
2025-05-27 11:02:20 -0300

    Adicionando as atividades da semana 4

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -8,6 +8,8 @@ import Atv02 from "./Atv02";
 
 import Atv03 from "./Atv03";
 
+import Atv04 from "./Atv04";
+
 export default function minhasRotas(){
     return(
         <BrowserRouter>
@@ -16,6 +18,7 @@ export default function minhasRotas(){
                 <Route path="atv01" element={<Atv01/>}/>
                 <Route path="atv02" element={<Atv02/>}/>
                 <Route path="atv03" element={<Atv03/>}/>
+                <Route path="atv04" element={<Atv04/>}/>
             </Routes>
         </BrowserRouter>
     );
